5d2a9a6f35df8f6bc5d32bfe5760310649fbf80e
juliqr <jalexa.qr@gmail.com>
2023-06-07 23:26:43 -0600

    107 sin errores

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -35,7 +35,7 @@ namespace API.Middleware
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
-                await context.Response.WriteAsJsonAsync(json);
+                await context.Response.WriteAsync(json);
             }
         }
     }
