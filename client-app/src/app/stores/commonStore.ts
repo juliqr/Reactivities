214a3e147d4416669145165535c7f2fa26f0442d
juliqr <jalexa.qr@gmail.com>
2023-06-30 20:50:38 -0600

    148 done

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,6 +3,8 @@ import { ServerError } from "../layout/models/serverError";
 
 export default class CommonStore {
   error: ServerError | null = null;
+  token: string | null = null;
+  appLoaded = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -11,4 +13,14 @@ export default class CommonStore {
   setServerError(error: ServerError) {
     this.error = error;
   }
+
+  //set the token
+  setToken = (token: string | null) => {
+    if (token) localStorage.setItem("jwt", token);
+    this.token = token; //equal to the token we have it inside our store state
+  };
+
+  setAppLoaded = () => {
+    this.appLoaded = true;
+  };
 }
